63dda474ec63548e798d6b33ee63fdd1bf16d8d7
TheGuja <jiag2023@gmail.com>
2025-08-27 10:50:29 -0400

    implemented finding the k nearest users to a specific userID

--- User Based Collaborative Filtering/userCollaborativeFiltering.py
+++ User Based Collaborative Filtering/userCollaborativeFiltering.py
@@ -14,3 +14,22 @@ ratings = pd.read_csv(ratingsFilepath, delimiter='::', engine='python')
 
 # Merge the datasets along MovieID column
 merged_df = movies.merge(ratings, on='MovieID')
+# print(merged_df.head())
+
+# Make user movie matrix
+userMovieMatrix = merged_df.pivot_table(index='UserID', columns='Title', values='Rating')
+# print(userMovieMatrix)
+
+# Make a similarity matrix
+similarityMatrix = userMovieMatrix.T.corr('pearson')
+# print(similarityMatrix)
+
+# Define the parameters
+topK = 10
+numRecommendations = 10
+similarityThreshold = 0.4
+userID = 100
+
+# Find k-nearest neighbors to userID
+kNN = similarityMatrix[similarityMatrix[userID] > similarityThreshold][userID].sort_values(ascending=False)[:topK]
+print(kNN)
