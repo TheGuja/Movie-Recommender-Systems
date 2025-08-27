190e611476acf2b20083a0bc2a5a1fe08cacc939
TheGuja <jiag2023@gmail.com>
2025-08-26 21:34:14 -0400

    added dataset imports aand merged dataframe

--- User Based Collaborative Filtering/userCollaborativeFiltering.py
+++ User Based Collaborative Filtering/userCollaborativeFiltering.py
@@ -1,1 +1,16 @@
 import pandas as pd
+from dotenv import load_dotenv
+import os
+
+# Load environment variables
+load_dotenv()
+movieFilepath = os.getenv('MOVIE_FILEPATH')
+ratingsFilepath = os.getenv('RATINGS_FILEPATH')
+
+# Import datasets
+movies = pd.read_csv(movieFilepath, delimiter='::', engine='python')
+ratings = pd.read_csv(ratingsFilepath, delimiter='::', engine='python')
+
+
+# Merge the datasets along MovieID column
+merged_df = movies.merge(ratings, on='MovieID')
